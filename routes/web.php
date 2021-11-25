17a6ef381d34ae2da4b2815702c8cd4ed11f7d67
SebatZaman007 <tasnimzamansebat@gmail.com>
2021-11-25 11:47:21 +0600

    ec-1

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dash.dashboard');
 });
