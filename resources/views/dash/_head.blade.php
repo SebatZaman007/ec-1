7b8b6c7715887c8d06763cf96aa15136e23316c6
SebatZaman007 <tasnimzamansebat@gmail.com>
2021-11-25 11:40:55 +0600

    first-commit

---
+++ resources/views/dash/_head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+<title>Pixie - Ecommerce HTML5 Template</title>
+
+<!-- Bootstrap core CSS -->
+<link href="{{asset('asset/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="{{asset('asset/css/fontawesome.css')}}">
+<link rel="stylesheet" href="{{asset('asset/css/tooplate-main.css')}}">
+<link rel="stylesheet" href="{{asset('asset/css/owl.css')}}">
